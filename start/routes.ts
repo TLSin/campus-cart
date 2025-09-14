cb788ad7c911fb1851d79426456c2f5a0ee8f6d8
drex1117 <jimboy.abad06@gmail.com>
2025-09-15 00:21:46 +0800

    Login/Signup

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,5 @@
 
 import router from '@adonisjs/core/services/router'
 router.on('/').renderInertia('home')
-
+router.on('/Login').renderInertia('Login')
+router.on('/Signup').renderInertia('Signup')
