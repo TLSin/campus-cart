0fec207d30f8f01519abc276b8ad62e844870ffa
drex1117 <jimboy.abad06@gmail.com>
2025-09-16 12:01:51 +0800

    Updated Login/Signup & Components

--- start/routes.ts
+++ start/routes.ts
@@ -11,3 +11,4 @@ import router from '@adonisjs/core/services/router'
 router.on('/').renderInertia('home')
 router.on('/Login').renderInertia('Login')
 router.on('/Signup').renderInertia('Signup')
+router.on('/homePage').renderInertia('homePage')
